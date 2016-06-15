8a8ab5c7fc6ff66f94b5523bfb9943be3529c21c
Brago <bragolab@gmail.com>
2016-06-15 11:34:36 +0200

    - Blob controller per controllo doppio blob.

---
+++ UnityProject/Assets/Project404/Scripts/BlobControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobControllerTest : MonoBehaviour {
+
+	public Animator animC1, animC2;
+
+	// Use this for initialization
+	void Start () {
+		animC1.speed = 1;
+		animC2.speed = 0.9f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
